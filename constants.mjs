380b7326a55c10278376d939e86686a20c592426
hokageCV <chaitanyavaru@gmail.com>
2023-06-02 14:15:33 +0530

    fix: stop multiple bookmarking of same url

--- constants.mjs
+++ constants.mjs
@@ -1,3 +1,4 @@
-export const extensionServerUrl = "https://fastify-extension.up.railway.app";
+export const extensionServerUrl = "http://localhost:4040";
+// export const extensionServerUrl = "https://fastify-extension.up.railway.app";
 
 export const AUTH_URL = "http://localhost:4000";
--- popup/popup.js
+++ popup/popup.js
@@ -2,9 +2,23 @@ import { getCurrentTabData } from "./getCurrentTabData.js";
 
 const bookmarkBtn = document.getElementById("bookmarkBtn");
 
+chrome.storage.local.get(["bookmarkList"], async (data) => {
+  const bookmarkList = data.bookmarkList;
+
+  const currentTabData = await getCurrentTabData();
+
+  if (bookmarkList.some((bookmark) => bookmark.url === currentTabData.url)) {
+    bookmarkBtn.disabled = true;
+    bookmarkBtn.innerText = "Already Bookmarked";
+  }
+});
+
 bookmarkBtn.addEventListener("click", async () => {
   const data = await getCurrentTabData();
 
   const message = { ...data, command: "add bookmark" };
   chrome.runtime.sendMessage(message);
+
+  bookmarkBtn.disabled = true;
+  bookmarkBtn.innerText = "Already Bookmarked";
 });
